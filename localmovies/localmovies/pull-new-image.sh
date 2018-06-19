643b6fa069c053877e14adf8ca661a640b76f857
nathan <rahm.nathan@gmail.com>
2018-06-18 23:47:00 -0500

    Removed out-dated shell script

--- localmovies/localmovies/pull-new-image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-cd /home/nathan/docker/localmovies/localmovies
-docker-compose pull localmovies && docker-compose up -d
