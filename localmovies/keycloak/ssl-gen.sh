032e41c17d76f8919cbadaaac3fda8735f482c53
nathan <rahm.nathan@gmail.com>
2018-07-09 21:28:43 -0500

    Simplified Keycloak deployment and upgraded to Keycloak 4

--- localmovies/keycloak/ssl-gen.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-keytool -genkey -keyalg RSA -dname "cn=Nathan Rahm, ou=localmovies, o=localmovies, c=US" -alias selfsigned -keystore /opt/jboss/keycloak/standalone/configuration/keystore.jks -storepass changeit -validity 360 -keysize 2048 -keypass changeit
