fc2d0be818a837cbd8a71450ccfcab6a619348e0
nathan <rahm.nathan@gmail.com>
2020-12-13 14:14:03 -0600

    Switched to Debian base image for hummingbird. Added 'persist-tun' option to hummingbird.

--- deluge/hummingbird/entrypoint.sh
+++ deluge/hummingbird/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 touch ./airvpn_log
-./hummingbird-linux-x86_64-1.1.0/hummingbird /config.ovpn -N off &> airvpn_log &
+./hummingbird-linux-x86_64-1.1.0/hummingbird /config.ovpn --persist-tun --network-lock off &> airvpn_log &
 
-sleep 20
+sleep 10
 
 echo "nameserver $(grep -oP "(?<=\[DNS\]\s\[)[\d\.]*" airvpn_log)" > /etc/resolv.conf
 echo "nameserver $(grep -oP "(?<=\[DNS6\]\s\[)[a-zA-Z\d\:]*" airvpn_log)" >> /etc/resolv.conf
