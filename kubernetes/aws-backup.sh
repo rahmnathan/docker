1dc02c6f720e704aa2e84dcc360fd3cd1e422aba
nathan <rahm.nathan@protonmail.com>
2026-01-01 16:39:25 -0800

    Update backup.

--- kubernetes/aws-backup.sh
+++ kubernetes/aws-backup.sh
@@ -3,4 +3,4 @@
 # Run on a cron to backup important persistence files to s3.
 # Does not include actual media files as they're too large (expensive/slow).
 
-aws s3 sync /srv/dev-disk-by-uuid-7575780d-60b1-4880-8bfb-542729db3165/PersistentVolumes s3://homelab-volume-backup-2 --exclude "*monitoring-elasticsearch*" --exclude "*monitoring-logstash*" --exclude "*monitoring-prometheus-kube-prometheus-stack-prometheus-db*"
+aws s3 sync /srv/dev-disk-by-uuid-7575780d-60b1-4880-8bfb-542729db3165/PersistentVolumes s3://homelab-volume-backup-2 --exclude "*monitoring-elasticsearch*" --exclude "*monitoring-logstash*" --exclude "*monitoring-prometheus-kube-prometheus-stack-prometheus-db*" --exclude "*swarm-ui-swarm-ui*" --exclude "*open-webui-open-webui*"
