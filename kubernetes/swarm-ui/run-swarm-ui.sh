b10bf997716152157c1103ef1336ac2a95253bfd
nathan <rahm.nathan@protonmail.com>
2026-07-11 21:05:39 -0700

    Swarm upgrade.

--- kubernetes/swarm-ui/run-swarm-ui.sh
+++ kubernetes/swarm-ui/run-swarm-ui.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-TAR_URL="https://artifactory.nathanrahm.com/artifactory/third-party/SwarmUI/SwarmUI.tar"
+TAR_URL="https://artifactory.nathanrahm.com/artifactory/third-party/SwarmUI/SwarmUI-0.9.8-Beta.tar.gz"
 TMP_TAR="/tmp/SwarmUI.tar"
 
 # Check if directory is empty
 if [ -z "$(ls -A "/SwarmUI/")" ]; then
     echo "Directory is empty — downloading and extracting..."
     curl -L "$TAR_URL" -o "$TMP_TAR"
-    tar -xzf "$TMP_TAR" -C "/"
+    tar --no-same-owner --strip-components=1 -xzf "$TMP_TAR" -C "/SwarmUI"
     echo "Extraction complete."
 else
     echo "Directory is not empty — skipping download/extraction."
